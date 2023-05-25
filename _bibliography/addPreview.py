b07de9fd72b77c83ec46cc67386a7581355ec389
Christoph Wagner <s2116812@ed.ac.uk>
2023-05-25 15:11:59 +0100

    Trying to fix missing authors

--- _bibliography/addPreview.py
+++ _bibliography/addPreview.py
@@ -12,7 +12,7 @@ for pprKey in bib_data.entries:
     if "keywords" in fieldsDict.keys():
         fieldsDict['preview'] = fieldsDict['keywords']
         fieldsDict.pop('keywords')
-    nuEntry = Entry(ppr.type, fields=fieldsDict)
+    nuEntry = Entry(ppr.type, fields=fieldsDict, persons=ppr.persons)
     nuParser.data.add_entry(pprKey, nuEntry)
 
 nuParser.data.to_file('papers.bib', bib_format='bibtex')
