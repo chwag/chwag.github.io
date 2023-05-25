d4ed6f9a784c203674e67210614434b2601952b1
Christoph Wagner <s2116812@ed.ac.uk>
2023-05-25 14:25:50 +0100

    Updated bibliography script

--- _bibliography/addPreview.py
+++ _bibliography/addPreview.py
@@ -8,13 +8,11 @@ for pprKey in bib_data.entries:
     print(pprKey)
     ppr = bib_data.entries[pprKey]
 
-    if 'note' in list(ppr.fields):
-        note = ppr.fields['note']
-        fieldsDict = dict(ppr.fields)
-        fieldsDict['preview'] = note
-        fieldsDict.pop('note')
-
-        nuEntry = Entry(ppr.type, fields=fieldsDict)
-        nuParser.data.add_entry(pprKey, nuEntry)
+    fieldsDict = dict(ppr.fields)
+    if "keywords" in fieldsDict.keys():
+        fieldsDict['preview'] = fieldsDict['keywords']
+        fieldsDict.pop('keywords')
+    nuEntry = Entry(ppr.type, fields=fieldsDict)
+    nuParser.data.add_entry(pprKey, nuEntry)
 
 nuParser.data.to_file('papers', bib_format='bibtex')
