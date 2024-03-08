7ceb4854f4f83f834a3b68d58f6db9f87bce2a0a
chwag <51704166+chwag@users.noreply.github.com>
2024-03-08 05:34:11 +0000

    Deploying to gh-pages from @ chwag/chwag.github.io@2ddf580d0b484b420015bdab0c18d16b91bb0325 🚀

--- assets/img/group/sizeReduction.py
+++ assets/img/group/sizeReduction.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, glob
 from PIL import Image
 
 infiles = glob.glob('*.png') + glob.glob('*.jpg')
@@ -9,6 +9,6 @@ for infile in infiles:
             outfile = os.path.splitext(infile)[0] + "_reduced"
             im = Image.open(infile)
             rgbIm = im.convert('RGB')
-            rgbIm.save(outfile + ".jpg", "JPEG", optimize=True)
+            rgbIm.save(outfile + ".jpg", "JPEG", optimize=True, quality=30)
         except IOError:
             print("cannot create thumbnail for '%s'" % infile)
